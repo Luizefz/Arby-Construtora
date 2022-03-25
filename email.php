0c3a8b146bc830df653ee161eb6395efdb445946
Lz-dvlp <luizefsilva2@gmail.com>
2022-03-25 17:33:42 -0300

    Finalização site

--- email.php
+++ email.php
@@ -6,12 +6,12 @@ $nome = addcslashes($_POST['name']);
 $email = addcslashes($_POST['email']);
 $mensagem = addcslashes($_POST['mesage']);
 
-$to = "arby@arbyconstrutora.com.br";
+$to = "@@@";
 $subject = "Email site - contado";
 $body = "Nome: ".$nome. "\r\n".
         "Email: ".$email. "\r\n".
         "Mensagem: ".$mensagem;
-$header = "From:arbyemail@arbyindustria.com.br"."\r\n".
+$header = "From:@@@"."\r\n".
             "Repley-To:".$email."\e\n".
             "X=Mailer:PHP/".phpversion();
 
